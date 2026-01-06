98cad874d7715931cf2fdae5f876441f097361ef
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-01-07 01:51:47 +0300

    Make PerformMultipleTriggerEffectsTriggerEffect

---
+++ Content/TriggerEffects/PerformMultipleTriggerEffectsTriggerEffect.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOStuffPack.Content.TriggerEffects
+{
+    public class PerformMultipleTriggerEffectsTriggerEffect(List<TriggerEffect> effects) : TriggerEffect
+    {
+        public List<TriggerEffect> effects = effects;
+        public bool preventMultipleManualPopups = true;
+        public bool forceNotManualPopups = false;
+
+        public override void DoEffect(IUnit sender, object args, TriggerEffectInfo triggerInfo, TriggerEffectActivationExtraInfo extraInfo)
+        {
+            if (ManuallyHandlePopup && preventMultipleManualPopups)
+                extraInfo = new()
+                {
+                    activation = extraInfo.activation,
+                    handler = new EvilTriggerEffectHandler(extraInfo.handler),
+                };
+
+            foreach (var effect in effects)
+                effect.DoEffect(sender, args, triggerInfo, extraInfo);
+        }
+
+        public override bool ManuallyHandlePopup => !forceNotManualPopups && effects.Any(x => x.ManuallyHandlePopup);
+
+        public class EvilTriggerEffectHandler(ITriggerEffectHandler orig) : ITriggerEffectHandler
+        {
+            public string DisplayedName => orig.DisplayedName;
+            public Sprite Sprite => orig.Sprite;
+
+            public bool didpopup = false;
+
+            public bool TryGetPopupUIAction(int unitId, bool isUnitCharacter, bool consumed, out CombatAction action)
+            {
+                if (didpopup)
+                {
+                    action = null;
+                    return false;
+                }
+
+                didpopup = true;
+                return orig.TryGetPopupUIAction(unitId, isUnitCharacter, consumed, out action);
+            }
+        }
+    }
+}
---
+++ Tools/LocalTriggerEffectTools.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOStuffPack.Tools
+{
+    public static class LocalTriggerEffectTools
+    {
+        public static TriggerEffect Add(this TriggerEffect one, TriggerEffect two)
+        {
+            if(one is not PerformMultipleTriggerEffectsTriggerEffect multiple)
+                return new PerformMultipleTriggerEffectsTriggerEffect([one, two]);
+
+            multiple.effects.Add(two);
+            return multiple;
+        }
+    }
+}
