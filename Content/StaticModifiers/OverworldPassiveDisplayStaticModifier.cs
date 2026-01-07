4024d7bb293febb4088cf441813a3500670439e5
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-01-07 23:28:43 +0300

    Add OverworldPassiveDisplayStaticModifier

---
+++ Content/StaticModifiers/OverworldPassiveDisplayStaticModifier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOStuffPack.Content.StaticModifiers
+{
+    [HarmonyPatch]
+    public class OverworldPassiveDisplayStaticModifier(List<BasePassiveAbilitySO> passives) : ItemModifierDataSetter
+    {
+        public List<BasePassiveAbilitySO> passives = passives;
+
+        [HarmonyPatch(typeof(CharacterInGameData), nameof(CharacterInGameData.UpdateCurrentPassives))]
+        [HarmonyPostfix]
+        public static void AddOverworldDisplayModifierPassives_Postfix(CharacterInGameData __instance)
+        {
+            if (!__instance.WearableModifiers.ModdedDataSetter.TryGetValue(nameof(OverworldPassiveDisplayStaticModifier), out var mod) || mod is not OverworldPassiveDisplayStaticModifier owPassiveDisplay)
+                return;
+
+            var currentPassives = __instance.CurrentPassives;
+            currentPassives.AddRange(
+                owPassiveDisplay.passives.FindAll(
+                    newPassive => !currentPassives.Exists(currPassive => currPassive.m_PassiveID == newPassive.m_PassiveID)));
+        }
+    }
+}
