e68e58ed5b6cab42ffee3c804cc242200ad81b52
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-01-08 00:12:32 +0300

    Add OverworldAbilityDisplayStaticModifier

---
+++ Content/StaticModifiers/OverworldAbilityDisplayStaticModifier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOStuffPack.Content.StaticModifiers
+{
+    [HarmonyPatch]
+    public class OverworldAbilityDisplayStaticModifier(List<CharacterAbility> abilties) : ItemModifierDataSetter
+    {
+        public List<CharacterAbility> abilties = abilties;
+
+        [HarmonyPatch(typeof(CharacterInGameData), nameof(CharacterInGameData.UpdateCurrentAbilities))]
+        [HarmonyPostfix]
+        public static void AddOverworldDisplayModifierAbilities_Postfix(CharacterInGameData __instance)
+        {
+            if (!__instance.WearableModifiers.ModdedDataSetter.TryGetValue(nameof(OverworldAbilityDisplayStaticModifier), out var mod) || mod is not OverworldAbilityDisplayStaticModifier owAbilityDisplay)
+                return;
+
+            __instance.CurrentAbilities.AddRange(owAbilityDisplay.abilties);
+        }
+    }
+}
