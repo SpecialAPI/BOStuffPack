9493c319de296faf7897242cfc43bc4ee5f83fc5
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-01-07 23:30:39 +0300

    Make the Shapeshifter display its passive addition in the overworld menu

--- Content/Items/InterdimensionalShapeshifter.cs
+++ Content/Items/InterdimensionalShapeshifter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BOStuffPack.Content.StaticModifiers;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -12,17 +13,18 @@ namespace BOStuffPack.Content.Items
             var flav = "\"Some see it as a pawn\"";
             var desc = "At the start of battle, add Shape-Shifter to this party member as a passive.";
 
-            var item = NewItem<MultiCustomTriggerEffectWearable>("InterdimensionalShapeShifter_TW")
-                .SetBasicInformation(name, flav, desc, "InterdimensionalShapeshifter")
-                .SetPrice(6)
-                .AddToTreasure()
-                .AddItemTypes(ItemType_GameIDs.Magic.ToString());
-
             var shapeshifter = NewPassive<MultiCustomTriggerEffectPassive>("ShapeShifter_PA", "ShapeShifter")
                 .SetBasicInformation("Shape-Shifter", "Shapeshifter")
                 .SetCharacterDescription("At the start of each turn, unequip this party member's held item and equip a random treasure item. Attempt to trigger that item's on combat start effects.")
                 .AddToGlossary("At the start of each turn, unequip this party member's held item and equip a random treasure item. Attempt to trigger that item's on combat start effects.")
                 .AddToDatabase();
+
+            var item = NewItem<MultiCustomTriggerEffectWearable>("InterdimensionalShapeShifter_TW")
+                .SetBasicInformation(name, flav, desc, "InterdimensionalShapeshifter")
+                .SetPrice(6)
+                .AddToTreasure()
+                .AddItemTypes(ItemType_GameIDs.Magic.ToString())
+                .SetStaticModifiers(ModdedDataModifier(new OverworldPassiveDisplayStaticModifier([shapeshifter])));
 
             shapeshifter.SetTriggerEffects(new()
             {
