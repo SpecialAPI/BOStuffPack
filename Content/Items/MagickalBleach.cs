88adebb81bba99a05c2c09e21870d9b123c605ac
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-01-26 02:58:01 +0300

    Rework Magickal Bleach

--- Content/Items/MagickalBleach.cs
+++ Content/Items/MagickalBleach.cs
@@ -10,15 +10,30 @@ namespace BOStuffPack.Content.Items
         public static void Init()
         {
             var name = "Magickal Bleach";
-            var flav = "\"A cure for death, inanimacy and your head being on fire. Just drink it and you're good to go!\"";
-            var desc = "This party member no longer has any passives.";
+            var flav = "\"Erase their mistakes.\"";
+            var desc = "At the start of combat, remove all passives from all party members.";
             
-            var item = NewItem<BasicWearable>("MagickalBleach_TW")
+            var item = NewItem<MultiCustomTriggerEffectWearable>("MagickalBleach_TW")
                 .SetBasicInformation(name, flav, desc, "MagickalBleach")
-                .SetStaticModifiers(ModdedDataModifier(new BleachStaticModifier()))
+                //.SetStaticModifiers(ModdedDataModifier(new BleachStaticModifier()))
                 .SetPrice(0)
                 .AddToTreasure()
                 .AddItemTypes(ItemType_GameIDs.Magic.ToString());
+
+            item.SetTriggerEffects(new()
+            {
+                new()
+                {
+                    trigger = TriggerCalls.OnCombatStart.ToString(),
+                    immediate = true,
+                    doesPopup = true,
+                    
+                    effect = new PerformEffectTriggerEffect(new()
+                    {
+                        Effects.GenerateEffect(CreateScriptable<RemoveAllPassives_Effect>(), 0, Targeting.Unit_AllAllies)
+                    })
+                }
+            });
         }
     }
 }
