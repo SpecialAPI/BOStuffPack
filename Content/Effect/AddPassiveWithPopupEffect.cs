7a91e24ff14a4d56daead1367cc855605cc2a283
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-02-13 22:10:36 +0300

    Add AddPassiveWithPopupEffect

---
+++ Content/Effect/AddPassiveWithPopupEffect.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOStuffPack.Content.Effect
+{
+    public class AddPassiveWithPopupEffect : EffectSO
+    {
+        public BasePassiveAbilitySO passive;
+
+        public override bool PerformEffect(CombatStats stats, IUnit caster, TargetSlotInfo[] targets, bool areTargetSlots, int entryVariable, out int exitAmount)
+        {
+            exitAmount = 0;
+
+            foreach(var t in targets)
+            {
+                if(t == null || !t.HasUnit)
+                    continue;
+
+                var u = t.Unit;
+                if(!u.AddPassiveAbility(passive))
+                    continue;
+
+                exitAmount++;
+                CombatManager.Instance.AddUIAction(new ShowPassiveInformationUIAction(u.ID, u.IsUnitCharacter, passive.GetPassiveLocData().text, passive.passiveIcon));
+            }
+
+            return exitAmount > 0;
+        }
+    }
+}
