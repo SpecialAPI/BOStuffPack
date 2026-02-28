4256c6ec69178c607b02964cd074bd94c7b859ca
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-02-28 20:30:28 +0300

    Global usings

--- GlobalUsings/GlobalUsings_Local.cs
+++ GlobalUsings/GlobalUsings_Local.cs
@@ -3,7 +3,14 @@
 global using static BOStuffPack.Plugin;
 
 global using BOStuffPack.Content.Effect;
+global using BOStuffPack.Content.Items;
+global using BOStuffPack.Content.Misc;
+global using BOStuffPack.Content.Passive;
+global using BOStuffPack.Content.StaticModifiers;
+global using BOStuffPack.Content.StoredValues;
+global using BOStuffPack.Content.Targets;
 global using BOStuffPack.Content.TriggerEffects;
+global using BOStuffPack.Tools;
 
 global using Priority = BrutalAPI.Priority;
 global using Status = BrutalAPI.StatusField;
