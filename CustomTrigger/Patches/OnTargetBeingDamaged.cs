d486f5c65a1b956afcce40f0c96f392af1b489f6
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-03-07 02:02:00 +0300

    Implement OnTargetBeingDamaged

---
+++ CustomTrigger/Patches/OnTargetBeingDamaged.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOStuffPack.CustomTrigger.Patches
+{
+    [HarmonyPatch]
+    internal static class OnTargetBeingDamaged
+    {
+        [HarmonyPatch(typeof(EnemyCombat), nameof(EnemyCombat.Damage))]
+        [HarmonyPatch(typeof(CharacterCombat), nameof(CharacterCombat.Damage))]
+        [HarmonyILManipulator]
+        private static void OnTargetBeingDamaged_Transpiler(ILContext ctx)
+        {
+            var crs = new ILCursor(ctx);
+
+            if (!crs.JumpToNext(x => x.MatchCallOrCallvirt<CombatManager>(nameof(CombatManager.PostNotification))))
+                return;
+
+            crs.Emit(OpCodes.Ldloc_2);
+            crs.EmitStaticDelegate(OnTargetBeingDamaged_TriggerEvent);
+        }
+
+        private static void OnTargetBeingDamaged_TriggerEvent(DamageReceivedValueChangeException ex)
+        {
+            if (ex.possibleSourceUnit is IUnit attacker)
+                CombatManager.Instance.PostNotification(LocalCustomTriggers.OnTargetBeingDamaged, attacker, ex);
+        }
+    }
+}
