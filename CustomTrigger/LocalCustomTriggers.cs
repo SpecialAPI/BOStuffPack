cf1267db5c17056bee3decca5fd31e6d47fcdcec
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-03-07 01:51:22 +0300

    Make LocalCustomTriggers more readable + nameof

--- CustomTrigger/LocalCustomTriggers.cs
+++ CustomTrigger/LocalCustomTriggers.cs
@@ -6,13 +6,13 @@ namespace BOStuffPack.CustomTrigger
 {
     public static class LocalCustomTriggers
     {
-        public static readonly string OnAnyoneFleetingEnd = $"{MOD_PREFIX}_OnAnyoneFleetingEnd";
-        public static readonly string OnAnyoneDamaged = $"{MOD_PREFIX}_OnAnyoneDamaged";
-        public static readonly string OnAnyoneDirectDamaged = $"{MOD_PREFIX}_OnAnyoneDirectDamaged";
-        public static readonly string OnAnyonesMaxHealthChanged = $"{MOD_PREFIX}_OnAnyonesMaxHealthChanged";
-        public static readonly string OnAnyoneHealed = $"{MOD_PREFIX}_OnAnyoneHealed";
-        public static readonly string ModifyCanProducePigmentFromDamage_Anyone = $"{MOD_PREFIX}_ModifyCanProducePigmentFromDamage_Anyone";
-        public static readonly string OnBeforeAbilityAnimation = $"{MOD_PREFIX}_OnBeforeAbilityAnimation";
-        public static readonly string OnPassivePopup = $"{MOD_PREFIX}_OnPassivePopup";
+        public static readonly string OnAnyoneFleetingEnd                       = $"{MOD_PREFIX}_{nameof(OnAnyoneFleetingEnd)}";
+        public static readonly string OnAnyoneDamaged                           = $"{MOD_PREFIX}_{nameof(OnAnyoneDamaged)}";
+        public static readonly string OnAnyoneDirectDamaged                     = $"{MOD_PREFIX}_{nameof(OnAnyoneDirectDamaged)}";
+        public static readonly string OnAnyonesMaxHealthChanged                 = $"{MOD_PREFIX}_{nameof(OnAnyonesMaxHealthChanged)}";
+        public static readonly string OnAnyoneHealed                            = $"{MOD_PREFIX}_{nameof(OnAnyoneHealed)}";
+        public static readonly string ModifyCanProducePigmentFromDamage_Anyone  = $"{MOD_PREFIX}_{nameof(ModifyCanProducePigmentFromDamage_Anyone)}";
+        public static readonly string OnBeforeAbilityAnimation                  = $"{MOD_PREFIX}_{nameof(OnBeforeAbilityAnimation)}";
+        public static readonly string OnPassivePopup                            = $"{MOD_PREFIX}_{nameof(OnPassivePopup)}";
     }
 }
