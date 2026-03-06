ee9f9fb1ae6b084752a71c6086e76df0f1a3e9c0
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-03-07 02:20:47 +0300

    Add PercentageModifierSetterTriggerEffect

---
+++ Content/TriggerEffects/PercentageModifierSetterTriggerEffect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOStuffPack.Content.TriggerEffects
+{
+    public class PercentageModifierSetterTriggerEffect(int percentage, bool increase) : TriggerEffect
+    {
+        public override void DoEffect(IUnit sender, object args, TriggerEffectInfo triggerInfo, TriggerEffectActivationExtraInfo extraInfo)
+        {
+            if (!ValueReferenceTools.TryGetValueChangeException(args, out var ex))
+                return;
+
+            ex.AddModifier(new PercentageValueModifier(ex.DamageDealt, percentage, increase));
+        }
+    }
+}
