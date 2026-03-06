cc58833f3334c8b7c1d63eaffc5b894860b6752e
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-03-07 01:54:54 +0300

    New triggers

--- CustomTrigger/LocalCustomTriggers.cs
+++ CustomTrigger/LocalCustomTriggers.cs
@@ -14,5 +14,7 @@ namespace BOStuffPack.CustomTrigger
         public static readonly string ModifyCanProducePigmentFromDamage_Anyone  = $"{MOD_PREFIX}_{nameof(ModifyCanProducePigmentFromDamage_Anyone)}";
         public static readonly string OnBeforeAbilityAnimation                  = $"{MOD_PREFIX}_{nameof(OnBeforeAbilityAnimation)}";
         public static readonly string OnPassivePopup                            = $"{MOD_PREFIX}_{nameof(OnPassivePopup)}";
+        public static readonly string OnTargetBeingDamaged                      = $"{MOD_PREFIX}_{nameof(OnTargetBeingDamaged)}";
+        public static readonly string OnTargetDamaged                           = $"{MOD_PREFIX}_{nameof(OnTargetDamaged)}";
     }
 }
