1c046a3e0c3e259334dd7851a6833be5b383e083
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-04-03 00:15:55 +0300

    Add ConsoleExcluder

---
+++ Tools/ConsoleExcluder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOStuffPack.Tools
+{
+    [HarmonyPatch]
+    public static class ConsoleExcluder
+    {
+        private static readonly List<string> excludedItems = [];
+
+        private static string LowerInvariant(string s) => s.ToLowerInvariant();
+
+        [HarmonyPatch(typeof(DebugController), "LoadItemIds", MethodType.Enumerator)]
+        [HarmonyILManipulator]
+        private static void ExcludeItems_Transpiler(ILContext ctx)
+        {
+            var crs = new ILCursor(ctx);
+
+            if (!crs.JumpToNext(x => x.MatchNewobj<List<string>>()))
+                return;
+
+            crs.EmitStaticDelegate(ExcludeItems_AddExcludedItems);
+        }
+
+        private static List<string> ExcludeItems_AddExcludedItems(List<string> processedList)
+        {
+            processedList.AddRange(excludedItems.Select(LowerInvariant));
+
+            return processedList;
+        }
+
+        public static T ExcludeFromConsole<T>(this T w) where T : BaseWearableSO
+        {
+            excludedItems.Add(w.name);
+
+            return w;
+        }
+    }
+}
