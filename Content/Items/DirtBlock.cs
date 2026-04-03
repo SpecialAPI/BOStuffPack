34e9dd6cb903c5dd5cff6fdb7cb7b4e735b226b4
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-04-03 16:28:19 +0300

    Add Dirt Block patch

--- Content/Items/DirtBlock.cs
+++ Content/Items/DirtBlock.cs
@@ -5,8 +5,12 @@ using System.Text;
 
 namespace BOStuffPack.Content.Items
 {
+    [HarmonyPatch]
     public static class DirtBlock
     {
+        public const int Chance = 157;
+        public const int MaxRoll = 1_000_000;
+
         public static void Init()
         {
             var name = "Dirt Block";
@@ -30,6 +34,33 @@ namespace BOStuffPack.Content.Items
                     effect = new BoolHolderSetterTriggerffect(false)
                 }
             });
+        }
+
+        [HarmonyPatch(typeof(RunDataSO), nameof(RunDataSO.GetPrizeItem))]
+        [HarmonyPatch(typeof(RunDataSO), nameof(RunDataSO.BuyShopItem))]
+        [HarmonyILManipulator]
+        public static void MaybeGiveDirtBlock_Transpiler(ILContext ctx)
+        {
+            var crs = new ILCursor(ctx);
+
+            if (!crs.JumpToNext(x => x.MatchCallOrCallvirt<PlayerInGameData>(nameof(PlayerInGameData.AddNewItem))))
+                return;
+
+            crs.Emit(OpCodes.Ldarg_0);
+            crs.EmitStaticDelegate(MaybeGiveDirtBlock_Give);
+        }
+
+        public static void MaybeGiveDirtBlock_Give(RunDataSO run)
+        {
+            var playerData = run.playerData;
+            if (!playerData.HasItemSpace)
+                return;
+
+            var roll = Random.Range(0, MaxRoll);
+            if (roll >= Chance)
+                return;
+
+            playerData.AddNewItem(GetWearable(Profile.GetID("DirtBlock_ExtraW")));
         }
     }
 }
