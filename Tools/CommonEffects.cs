ffb2e2b1523a67e57e1870cf57d0a9596d0526e3
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-05-17 17:03:08 +0300

    CommonEffects

---
+++ Tools/CommonEffects.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOStuffPack.Tools
+{
+    public static class CommonEffects
+    {
+        public static EffectSO Damage = CreateScriptable<DamageEffect>();
+        public static EffectSO IndirecDamage = CreateScriptable<DamageEffect>(x => x._indirect = true);
+        public static EffectSO SwapLeft = CreateScriptable<SwapToOneSideEffect>(x => x._swapRight = false);
+        public static EffectSO SwapRight = CreateScriptable<SwapToOneSideEffect>(x => x._swapRight = true);
+        public static EffectSO SwapSides = CreateScriptable<SwapToSidesEffect>();
+
+        public static EffectSO ApplyFocused = ApplyStatus(StatusField.Focused);
+        public static EffectSO ApplyRuptured = ApplyStatus(StatusField.Ruptured);
+        public static EffectSO ApplyFrail = ApplyStatus(StatusField.Frail);
+        public static EffectSO ApplyOilSlicked = ApplyStatus(StatusField.OilSlicked);
+        public static EffectSO ApplySpotlight = ApplyStatus(StatusField.Spotlight);
+        public static EffectSO ApplyCursed = ApplyStatus(StatusField.Cursed);
+        public static EffectSO ApplyLinked = ApplyStatus(StatusField.Linked);
+        public static EffectSO ApplyDivineProtection = ApplyStatus(StatusField.Focused);
+        public static EffectSO ApplyScars = ApplyStatus(StatusField.Scars);
+        public static EffectSO ApplyGutted = ApplyStatus(StatusField.Gutted);
+        public static EffectSO ApplyStunned = ApplyStatus(StatusField.Stunned);
+
+        public static EffectSO ApplyConstricted = ApplyField(StatusField.Constricted);
+        public static EffectSO ApplyFire = ApplyField(StatusField.OnFire);
+        public static EffectSO ApplyShield = ApplyField(StatusField.Shield);
+
+        public static EffectSO ApplyStatus(StatusEffect_SO status)
+        {
+            var e = CreateScriptable<StatusEffect_Apply_Effect>();
+            e._Status = status;
+
+            return e;
+        }
+
+        public static EffectSO ApplyField(FieldEffect_SO field)
+        {
+            var e = CreateScriptable<FieldEffect_Apply_Effect>();
+            e._Field = field;
+
+            return e;
+        }
+
+        public static EffectSO PlayAnimation(AttackVisualsSO visuals)
+        {
+            var e = CreateScriptable<AnimationVisualsOnEffectTargetsEffect>();
+            e.visuals = Visuals.Mitosis;
+
+            return e;
+        }
+    }
+}
