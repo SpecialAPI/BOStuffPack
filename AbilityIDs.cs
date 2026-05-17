27ec89cb66ae5cb2960a3d52f591ea8c8eac1d70
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-05-17 22:52:09 +0300

    Prefix

--- AbilityIDs.cs
+++ AbilityIDs.cs
@@ -6,12 +6,12 @@ namespace BOStuffPack
 {
     public static class AbilityIDs
     {
-        public static readonly string Anchor                    = "Anchor_A";
-        public static readonly string TwoIntoOne                = "TwoIntoOne_A";
-        public static readonly string EndOfTheUniverse          = "EndOfTheUniverse_A";
-        public static readonly string Key                       = "Key{0}_A";
-        public static readonly string Lock                      = "Lock{0}_A";
-        public static readonly string MurderEveryoneYouKnow     = "MurderEveryoneYouKnow_A";
-        public static readonly string Intimidation              = "Intimidation_A";
+        public static readonly string Anchor                    = "Anchor_A".Prefix();
+        public static readonly string TwoIntoOne                = "TwoIntoOne_A".Prefix();
+        public static readonly string EndOfTheUniverse          = "EndOfTheUniverse_A".Prefix();
+        public static readonly string Key                       = "Key{0}_A".Prefix();
+        public static readonly string Lock                      = "Lock{0}_A".Prefix();
+        public static readonly string MurderEveryoneYouKnow     = "MurderEveryoneYouKnow_A".Prefix();
+        public static readonly string Intimidation              = "Intimidation_A".Prefix();
     }
 }
--- StoredValueIDs.cs
+++ StoredValueIDs.cs
@@ -6,31 +6,31 @@ namespace BOStuffPack
 {
     public static class StoredValueIDs
     {
-        public static readonly string MergedCountDB                 = "MergedCount_USD";
-        public static readonly string MergedCountID                 = "MergedCount";
-        public static readonly string KeybladeRTurnDB               = "KeybladeRTurn_USD";
-        public static readonly string KeybladeRTurnID               = "KeybladeRTurn";
-        public static readonly string KeybladeBTurnDB               = "KeybladeBTurn_USD";
-        public static readonly string KeybladeBTurnID               = "KeybladeBTurn";
-        public static readonly string KeybladeYTurnDB               = "KeybladeYTurn_USD";
-        public static readonly string KeybladeYTurnID               = "KeybladeYTurn";
-        public static readonly string KeybladePTurnDB               = "KeybladePTurn_USD";
-        public static readonly string KeybladePTurnID               = "KeybladePTurn";
-        public static readonly string BloodDB                       = "Blood_USD";
-        public static readonly string BloodID                       = "Blood";
-        public static readonly string UnnamedItem5DB                = "UnnamedItem5_USD";
-        public static readonly string UnnamedItem5ID                = "UnnamedItem5";
-        public static readonly string UnnamedItem42TempDisableDB    = "UnnamedItem42TempDisable_USD";
-        public static readonly string UnnamedItem42TempDisableID    = "UnnamedItem42TempDisable";
-        public static readonly string FramedEDB                     = "FramedE_USD";
-        public static readonly string FramedEID                     = "FramedE";
-        public static readonly string MergingStonesDB               = "MergingStones_USD";
-        public static readonly string MergingStonesID               = "MergingStones";
-        public static readonly string BlankBookAbilityDB            = "BlankBook_Ability_USD";
-        public static readonly string BlankBookAbilityID            = "BlankBook_Ability";
-        public static readonly string BlankBookPassiveDB            = "BlankBook_Passive_USD";
-        public static readonly string BlankBookPassiveID            = "BlankBook_Passive";
-        public static readonly string UnnamedItem31DB               = "UnnamedItem31_USD";
-        public static readonly string UnnamedItem31ID               = "UnnamedItem31";
+        public static readonly string MergedCountDB                 = "MergedCount_USD".Prefix();
+        public static readonly string MergedCountID                 = "MergedCount".Prefix();
+        public static readonly string KeybladeRTurnDB               = "KeybladeRTurn_USD".Prefix();
+        public static readonly string KeybladeRTurnID               = "KeybladeRTurn".Prefix();
+        public static readonly string KeybladeBTurnDB               = "KeybladeBTurn_USD".Prefix();
+        public static readonly string KeybladeBTurnID               = "KeybladeBTurn".Prefix();
+        public static readonly string KeybladeYTurnDB               = "KeybladeYTurn_USD".Prefix();
+        public static readonly string KeybladeYTurnID               = "KeybladeYTurn".Prefix();
+        public static readonly string KeybladePTurnDB               = "KeybladePTurn_USD".Prefix();
+        public static readonly string KeybladePTurnID               = "KeybladePTurn".Prefix();
+        public static readonly string BloodDB                       = "Blood_USD".Prefix();
+        public static readonly string BloodID                       = "Blood".Prefix();
+        public static readonly string UnnamedItem5DB                = "UnnamedItem5_USD".Prefix();
+        public static readonly string UnnamedItem5ID                = "UnnamedItem5".Prefix();
+        public static readonly string UnnamedItem42TempDisableDB    = "UnnamedItem42TempDisable_USD".Prefix();
+        public static readonly string UnnamedItem42TempDisableID    = "UnnamedItem42TempDisable".Prefix();
+        public static readonly string FramedEDB                     = "FramedE_USD".Prefix();
+        public static readonly string FramedEID                     = "FramedE".Prefix();
+        public static readonly string MergingStonesDB               = "MergingStones_USD".Prefix();
+        public static readonly string MergingStonesID               = "MergingStones".Prefix();
+        public static readonly string BlankBookAbilityDB            = "BlankBook_Ability_USD".Prefix();
+        public static readonly string BlankBookAbilityID            = "BlankBook_Ability".Prefix();
+        public static readonly string BlankBookPassiveDB            = "BlankBook_Passive_USD".Prefix();
+        public static readonly string BlankBookPassiveID            = "BlankBook_Passive".Prefix();
+        public static readonly string UnnamedItem31DB               = "UnnamedItem31_USD".Prefix();
+        public static readonly string UnnamedItem31ID               = "UnnamedItem31".Prefix();
     }
 }
