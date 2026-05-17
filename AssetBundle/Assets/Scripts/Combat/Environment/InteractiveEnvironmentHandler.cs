5fba25a6f322a01d85cbb0933f51ebef2a67ebf3
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-05-17 16:07:00 +0300

    Remove asset bundle

--- AssetBundle/Assets/Scripts/Combat/Environment/InteractiveEnvironmentHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class InteractiveEnvironmentHandler : MonoBehaviour
-{
-    public abstract void SetUpNotifications();
-    public abstract void TriggerEnvironment(IExtraCombatAmbienceOptions audioOptions, int id);
-}
