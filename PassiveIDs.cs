b503c22083ee48187ac3545dd0ed42c8bd40021a
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-05-17 22:52:20 +0300

    PassiveIDs
    im so bored....

---
+++ PassiveIDs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOStuffPack
+{
+    public static class PassiveIDs
+    {
+        public static readonly string MergedDB = "Merged_PA".Prefix();
+        public static readonly string MergedID = "Merged".Prefix();
+        public static readonly string ShapeShifterDB = "ShapeShifter_PA".Prefix();
+        public static readonly string ShapeShifterID = "ShapeShifter".Prefix();
+    }
+}
