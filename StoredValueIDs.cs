d8564e555807ce390c1793a63504d29b27541f17
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-05-17 22:38:12 +0300

    StoredValueIDs

---
+++ StoredValueIDs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOStuffPack
+{
+    public static class StoredValueIDs
+    {
+        public static readonly string MergedCountDB                 = "MergedCount_USD";
+        public static readonly string MergedCountID                 = "MergedCount";
+        public static readonly string KeybladeRTurnDB               = "KeybladeRTurn_USD";
+        public static readonly string KeybladeRTurnID               = "KeybladeRTurn";
+        public static readonly string KeybladeBTurnDB               = "KeybladeBTurn_USD";
+        public static readonly string KeybladeBTurnID               = "KeybladeBTurn";
+        public static readonly string KeybladeYTurnDB               = "KeybladeYTurn_USD";
+        public static readonly string KeybladeYTurnID               = "KeybladeYTurn";
+        public static readonly string KeybladePTurnDB               = "KeybladePTurn_USD";
+        public static readonly string KeybladePTurnID               = "KeybladePTurn";
+        public static readonly string BloodDB                       = "Blood_USD";
+        public static readonly string BloodID                       = "Blood";
+        public static readonly string UnnamedItem5DB                = "UnnamedItem5_USD";
+        public static readonly string UnnamedItem5ID                = "UnnamedItem5";
+        public static readonly string UnnamedItem42TempDisableDB    = "UnnamedItem42TempDisable_USD";
+        public static readonly string UnnamedItem42TempDisableID    = "UnnamedItem42TempDisable";
+        public static readonly string FramedEDB                     = "FramedE_USD";
+        public static readonly string FramedEID                     = "FramedE";
+        public static readonly string MergingStonesDB               = "MergingStones_USD";
+        public static readonly string MergingStonesID               = "MergingStones";
+        public static readonly string BlankBookAbilityDB            = "BlankBook_Ability_USD";
+        public static readonly string BlankBookAbilityID            = "BlankBook_Ability";
+        public static readonly string BlankBookPassiveDB            = "BlankBook_Passive_USD";
+        public static readonly string BlankBookPassiveID            = "BlankBook_Passive";
+        public static readonly string UnnamedItem31DB               = "UnnamedItem31_USD";
+        public static readonly string UnnamedItem31ID               = "UnnamedItem31";
+    }
+}
