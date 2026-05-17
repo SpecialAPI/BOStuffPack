81261b4ae23796d50352fed44acc6c6a6b39aa8e
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-05-17 22:43:38 +0300

    AbilityIDs

---
+++ AbilityIDs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOStuffPack
+{
+    public static class AbilityIDs
+    {
+        public static readonly string Anchor                    = "Anchor_A";
+        public static readonly string TwoIntoOne                = "TwoIntoOne_A";
+        public static readonly string EndOfTheUniverse          = "EndOfTheUniverse_A";
+        public static readonly string Key                       = "Key{0}_A";
+        public static readonly string Lock                      = "Lock{0}_A";
+        public static readonly string MurderEveryoneYouKnow     = "MurderEveryoneYouKnow_A";
+        public static readonly string Intimidation              = "Intimidation_A";
+    }
+}
