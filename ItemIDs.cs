0877d2129e0ae03768d292b7da3f297203bd77fc
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-05-17 22:16:19 +0300

    ItemIDs class

---
+++ ItemIDs.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOStuffPack
+{
+    public static class ItemIDs
+    {
+        public static readonly string TheTideTurner                 = "TheTideTurner_TW".Prefix();
+        public static readonly string BloodyHacksaw                 = "BloodyHacksaw_SW".Prefix();
+        public static readonly string ConjoinedFungi                = "ConjoinedFungi_TW".Prefix();
+        public static readonly string RipAndTear                    = "RipAndTear_TW".Prefix();
+        public static readonly string SquirrelBomb                  = "SquirrelBomb_TW".Prefix();
+        public static readonly string Survivorship                  = "Survivorship_TW".Prefix();
+        public static readonly string LoudPhone                     = "LoudPhone_TW".Prefix();
+        public static readonly string MagickalBleach                = "MagickalBleach_TW".Prefix();
+        public static readonly string Potential                     = "Potential_TW".Prefix();
+        public static readonly string NewtonsApple                  = "NewtonsApple_TW".Prefix();
+        public static readonly string InterdimensionalShapeshifter  = "InterdimensionalShapeShifter_TW".Prefix();
+        public static readonly string WorldShatter                  = "WorldShatter_TW".Prefix();
+        public static readonly string Keyring                       = "Keyring_TW".Prefix();
+        public static readonly string Pencil                        = "Pencil_TW".Prefix();
+        public static readonly string AlmightyBranch                = "AlmightyBranch_TW".Prefix();
+        public static readonly string Bait                          = "Bait_TW".Prefix();
+        public static readonly string MergingStones                 = "MergingStones_TW".Prefix();
+        //public static readonly string UnnamedItem17                 = "".Prefix();
+        public static readonly string PaperCrown                    = "PaperCrown_TW".Prefix();
+        public static readonly string BlueCrown                     = "BlueCrown_TW".Prefix();
+        public static readonly string InstrumentsOfMurder           = "InstrumentsOfMurder_TW".Prefix();
+        public static readonly string FramedE                       = "FramedE_TW".Prefix();
+        public static readonly string RedMarker                     = "RedMarker_TW".Prefix();
+        public static readonly string Electromagnet                 = "Electromagnet_SW".Prefix();
+        public static readonly string Bookmark                      = "Bookmark_SW".Prefix();
+        public static readonly string UnnamedItem1                  = "UnnamedItem1_SW".Prefix();
+        public static readonly string TheHumanCondition             = "TheHumanCondition_TW".Prefix();
+        public static readonly string BlankBook                     = "BlankBook_TW".Prefix();
+        public static readonly string WrittenBook                   = "WrittenBook_ExtraW".Prefix();
+        public static readonly string Blasphemy                     = "Blasphemy_TW".Prefix();
+        public static readonly string UnnamedItem31                 = "UnnamedItem31_SW".Prefix();
+        public static readonly string DiceBullets                   = "DiceBullets_SW".Prefix();
+        public static readonly string UnnamedItem5                  = "UnnamedItem5_TW".Prefix();
+        public static readonly string UnnamedItem35                 = "UnnamedItem35_TW".Prefix();
+        //public static readonly string UnnamedItem36                 = "".Prefix();
+        public static readonly string UnnamedItem37                 = "UnnamedItem37".Prefix();
+        public static readonly string FakeGun                       = "FakeGun_TW".Prefix();
+        public static readonly string UnnamedItem42                 = "UnnamedItem42_TW".Prefix();
+        public static readonly string DirtBlock                     = "DirtBlock_ExtraW".Prefix();
+        public static readonly string OldBleach                     = "OldBleach_ExtraW".Prefix();
+        public static readonly string TrueZeal                      = "TrueZeal_ExtraW".Prefix();
+    }
+}
