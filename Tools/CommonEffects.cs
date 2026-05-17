7d639ea9af511f25e68e6e349d870342d1de2f01
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-05-17 17:05:18 +0300

    Add more effects to CommonEffects

--- Tools/CommonEffects.cs
+++ Tools/CommonEffects.cs
@@ -6,27 +6,33 @@ namespace BOStuffPack.Tools
 {
     public static class CommonEffects
     {
-        public static EffectSO Damage = CreateScriptable<DamageEffect>();
-        public static EffectSO IndirecDamage = CreateScriptable<DamageEffect>(x => x._indirect = true);
-        public static EffectSO SwapLeft = CreateScriptable<SwapToOneSideEffect>(x => x._swapRight = false);
-        public static EffectSO SwapRight = CreateScriptable<SwapToOneSideEffect>(x => x._swapRight = true);
-        public static EffectSO SwapSides = CreateScriptable<SwapToSidesEffect>();
+        public static readonly EffectSO Damage = CreateScriptable<DamageEffect>();
+        public static readonly EffectSO IndirecDamage = CreateScriptable<DamageEffect>(x => x._indirect = true);
 
-        public static EffectSO ApplyFocused = ApplyStatus(StatusField.Focused);
-        public static EffectSO ApplyRuptured = ApplyStatus(StatusField.Ruptured);
-        public static EffectSO ApplyFrail = ApplyStatus(StatusField.Frail);
-        public static EffectSO ApplyOilSlicked = ApplyStatus(StatusField.OilSlicked);
-        public static EffectSO ApplySpotlight = ApplyStatus(StatusField.Spotlight);
-        public static EffectSO ApplyCursed = ApplyStatus(StatusField.Cursed);
-        public static EffectSO ApplyLinked = ApplyStatus(StatusField.Linked);
-        public static EffectSO ApplyDivineProtection = ApplyStatus(StatusField.Focused);
-        public static EffectSO ApplyScars = ApplyStatus(StatusField.Scars);
-        public static EffectSO ApplyGutted = ApplyStatus(StatusField.Gutted);
-        public static EffectSO ApplyStunned = ApplyStatus(StatusField.Stunned);
+        public static readonly EffectSO Heal = CreateScriptable<HealEffect>();
 
-        public static EffectSO ApplyConstricted = ApplyField(StatusField.Constricted);
-        public static EffectSO ApplyFire = ApplyField(StatusField.OnFire);
-        public static EffectSO ApplyShield = ApplyField(StatusField.Shield);
+        public static readonly EffectSO SwapLeft = CreateScriptable<SwapToOneSideEffect>(x => x._swapRight = false);
+        public static readonly EffectSO SwapRight = CreateScriptable<SwapToOneSideEffect>(x => x._swapRight = true);
+        public static readonly EffectSO SwapSides = CreateScriptable<SwapToSidesEffect>();
+
+        public static readonly EffectSO Refresh = CreateScriptable<RefreshAbilityUseEffect>();
+        public static readonly EffectSO RestoreSwap = CreateScriptable<RestoreSwapUseEffect>();
+
+        public static readonly EffectSO ApplyFocused = ApplyStatus(StatusField.Focused);
+        public static readonly EffectSO ApplyRuptured = ApplyStatus(StatusField.Ruptured);
+        public static readonly EffectSO ApplyFrail = ApplyStatus(StatusField.Frail);
+        public static readonly EffectSO ApplyOilSlicked = ApplyStatus(StatusField.OilSlicked);
+        public static readonly EffectSO ApplySpotlight = ApplyStatus(StatusField.Spotlight);
+        public static readonly EffectSO ApplyCursed = ApplyStatus(StatusField.Cursed);
+        public static readonly EffectSO ApplyLinked = ApplyStatus(StatusField.Linked);
+        public static readonly EffectSO ApplyDivineProtection = ApplyStatus(StatusField.Focused);
+        public static readonly EffectSO ApplyScars = ApplyStatus(StatusField.Scars);
+        public static readonly EffectSO ApplyGutted = ApplyStatus(StatusField.Gutted);
+        public static readonly EffectSO ApplyStunned = ApplyStatus(StatusField.Stunned);
+
+        public static readonly EffectSO ApplyConstricted = ApplyField(StatusField.Constricted);
+        public static readonly EffectSO ApplyFire = ApplyField(StatusField.OnFire);
+        public static readonly EffectSO ApplyShield = ApplyField(StatusField.Shield);
 
         public static EffectSO ApplyStatus(StatusEffect_SO status)
         {
