f4ed80485d5976f8b65a1167bb793640805e5e67
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-08-22 17:37:28 +0300

    Update bobuild.bat

--- bobuild.bat
+++ bobuild.bat
@@ -1,23 +1,36 @@
 set "modfolder=BOStuffPack"
 set "steampath=%programfiles(x86)%\Steam"
 set "modinfofolder=ModInfo"
+set "dllfolder=plugins"
 
 ::===================================================================
 set "modpath=%steampath%\steamapps\common\Brutal Orchestra\BrutalOrchestra_Data\StreamingAssets\mods\%modfolder%"
+set "dlldest=%modpath%\%dllfolder%"
+echo Mod folder: %modpath%
+
+::FOLDERS
+if not exist "%modpath%" (
+    echo Creating mod folder...
+    md "%modpath%"
+)
+if not exist "%dlldest%" (
+    echo Creating %dllfolder% folder...
+    md "%dlldest%"
+)
 
 ::DLL
 set "dllname=%assemblyname%.dll"
-echo Copying DLL
-copy "%outdir%%dllname%" "%modpath%\plugins\%dllname%"
+echo Copying DLL...
+copy "%outdir%%dllname%" "%dlldest%\%dllname%"
 
 ::MODINFO
 if exist "%modinfofolder%" (
     if exist "%modinfofolder%\modinfo.config" (
-        echo Copying modinfo.config
+        echo Copying modinfo.config...
         copy "%modinfofolder%\modinfo.config" "%modpath%\modinfo.config"
     )
     if exist "%modinfofolder%\thumbnail.png" (
-        echo Copying thumbnail
+        echo Copying thumbnail...
         copy "%modinfofolder%\thumbnail.png" "%modpath%\thumbnail.png"
     )
 )
